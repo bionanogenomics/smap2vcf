360b00da4244be3337c60cbb0e927e69b8287f83
BionanoDev <dev@bionanogenomics.com>
2017-08-15 14:18:30 -0700

    Update smap_to_vcf_v2.py

--- smap_to_vcf_v2.py
+++ smap_to_vcf_v2.py
@@ -2,6 +2,7 @@
 
 #Version 1 supports conversion of insertions and deletions
 #Version 2 supports conversion of translocation breakpoints
+#Version 3 supports conversion of inversion and duplication breakpoints
 
 description='''Stand-alone script to convert BioNano smap file format to vcf.'''
 
@@ -53,6 +54,8 @@ vcfheader_2='''##fileformat=VCFv4.2
 ##ALT=<ID=DEL,Description="Deletion">
 ##ALT=<ID=INS,Description="Insertion">
 ##ALT=<ID=TRA,Description="Translocation breakpoint">
+##ALT=<ID=INV,Description="Inversion">
+##ALT=<ID=DUP,Description="Duplication">
 ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">'''
 
 import os
@@ -67,7 +70,12 @@ class SV(object):
         self.vcf_type = vcf_type
         self.svlen = svlen #default SV length
 
-sv_types = [SV("insertion", "INS"), SV("deletion","DEL"), SV("translocation_intrachr","TRA",0),SV("translocation_interchr","TRA",0)] #conversion table
+#conversion table
+sv_types = [SV("insertion", "INS"), SV("deletion","DEL"),
+            SV("translocation_intrachr","TRA",0),
+            SV("translocation_interchr","TRA",0),
+            SV("inversion", "INV"), SV("inversion_partial", "INV"), SV("inversion_paired", "INV"),
+            SV("duplication", "DUP"), SV("duplication_split", "DUP"), SV("duplication_inverted", "INVDUP")]
 accepted_svtypes = dict([(sv.smap_type,sv.vcf_type) for sv in sv_types])
 default_svlen = dict([(sv.smap_type,sv.svlen) for sv in sv_types])
 
@@ -160,8 +168,11 @@ def smap_to_vcf(smappath, refcmap, xmappath, sample, output_prefix, ref_accessio
 
     f1 = open(smappath)
     dogeno = False #smap has genotype info: add to vcf
+    docoord = False
     nent = 0 #number of processed entries
+    linen = 0 #in case first entry is inversion, nent is not incremented, but need to prevent header from being written twice
     conf = defaultconf #if !doconf, this is used
+    invdata = {}
     for line in f1 :
         if line[0] == "#" :
             if '# Reference Maps From:' in line:
@@ -178,7 +189,7 @@ def smap_to_vcf(smappath, refcmap, xmappath, sample, output_prefix, ref_accessio
             continue
         vcftype = accepted_svtypes[svtype]
 
-        if nent == 0 :
+        if linen == 0 :
             if len(tokens) > 19 : #at least 20 columns for genotype
                 dogeno = True
                 fout.write(reference_header)
@@ -186,6 +197,7 @@ def smap_to_vcf(smappath, refcmap, xmappath, sample, output_prefix, ref_accessio
                 fout.write(colhead+colform)
             else : #no genotype
                 fout.write(colhead+"\n")
+        linen += 1
 
         smapid = tokens[0] #SmapEntryID
         #qry = tokens[1] #QryContigID
@@ -201,13 +213,12 @@ def smap_to_vcf(smappath, refcmap, xmappath, sample, output_prefix, ref_accessio
             try :
                 conf = float(conf)
                 #I was going to put this if block in an else of the try, but this can also catch exceptions raised in log10
-
                 if conf==-1.0 :
                     conf = defaultconf
                 elif conf >= 1.0 :
                     conf = maxconf
                 else :
-                    conf = str(round(-10*math.log10(1-conf),2))
+                    conf = str(abs(round(-10*math.log10(1-conf),2))) #abs to prevent '-0.0'
             except :
                 conf = defaultconf
         #9: type (above); 10, 11: xmapids; 12: Linkid; 13, 14: Qry Idxs
@@ -224,11 +235,12 @@ def smap_to_vcf(smappath, refcmap, xmappath, sample, output_prefix, ref_accessio
                 gt = "0/1"
 
         refstartpos = refcmap[ref][refstartidx-1] #if start site is misresolved
-        refstoppos  = refcmap[ref1][refstopidx-1] #same for end
+        if ref1 > 0 : #inversion_partial have -1 for RefcontigID2 and RefEndPos
+            refstoppos = refcmap[ref1][refstopidx-1] #same for end
         #print refstartpos, refstart, #debug
 
-        #check for the wrong reference for insertions and deletions
         if svtype=="insertion" or svtype=="deletion":
+            #check for the wrong reference for insertions and deletions
             if refstartpos > refstop or refstoppos < refstart :
                 print "WARNING: incompatible site positons from coordinates (%.1f - %.1f) and indices (%.1f - %.1f): please check reference cmap" % (refstart, refstop, refstartpos, refstoppos)
 
@@ -245,7 +257,7 @@ def smap_to_vcf(smappath, refcmap, xmappath, sample, output_prefix, ref_accessio
             else :
                 prevrefstop = refcmap[ref1][refstopidx-3]
             #print prevrefstop #debug
-            end = round( (prevrefstop + refstop)/2 )+1
+            end = round( (prevrefstop + refstop)/2 ) #+1
             ciend = round( (refstop - prevrefstop)/2 )
 
             if svtype=="insertion": #special case where ref start and end are the same for insertions
@@ -258,8 +270,10 @@ def smap_to_vcf(smappath, refcmap, xmappath, sample, output_prefix, ref_accessio
                 print line.strip()
                 print refstartpos, refstart, refstoppos, refstop
                 print refstart, nextrefstart, prevrefstop, refstop
-            if pos==end:
+            elif pos==end:
                 print "WARNING: start position (%.1f) is equal to end position (%.1f)" % (pos, end)
+
+            svlen = int(round( abs( abs(refstop-refstart) - abs(qrystop-qrystart) ) )) * (1. if svtype == "insertion" else -1.)
 
         elif svtype=="translocation_intrachr" or svtype=="translocation_interchr":
 
@@ -306,6 +320,91 @@ def smap_to_vcf(smappath, refcmap, xmappath, sample, output_prefix, ref_accessio
             else:
                 print "WARNING: orientation combination not recognized"
 
+            svlen = default_svlen[svtype]
+            docoord = True
+            
+        elif svtype == "inversion" or (svtype == "inversion_paired" and not invdata) :
+            #should never happen for well-formed smap: should be cleared below, but for paired, first and second are same type, so this check isn't possible without checking LinkID
+            if svtype == "inversion" and invdata : 
+                print "WARNING: inversion data not cleared (entry %s)" % line.strip()
+            
+            #note: start/stop are not sorted in smap
+            invdata = {"refstart"   :min(refstart,    refstop),   
+                       "refstartpos":min(refstartpos, refstoppos),
+                       "refstartidx":min(refstartidx, refstopidx),
+                       "refstop"    :max(refstart,    refstop),   
+                       "refstoppos" :max(refstartpos, refstoppos),
+                       "refstopidx" :max(refstartidx, refstopidx) }
+            #do NOT output at this stage: need the partial or other paired entry, which must be next line, or at least before next inversion
+            continue
+
+        elif svtype == "inversion_partial" or svtype == "inversion_paired" :
+            #note: this must match the last 'inversion' entry, but this is not checked (should be guaranteed by smap)
+            if not invdata :
+                #skip silently in case this is not a filtered smap: for unfiltered, inversion_repeat will cause this warning, but those are filtered in the Pipeline.
+                #print "WARNING: missing inversion data (entry %s); skipping" % line.strip()
+                continue
+
+            ref1 = ref
+            #partials always have refstart filled and refend empty, but need to pick start/end based on
+            # the inversion entry. partial start must be min OR max. for paired, no sorting is guaranteed
+            if svtype == "inversion_partial" and invdata["refstart"] < refstart and refstart < invdata["refstop"] :
+                print "WARNING: invalid inversion data (entry %s); skipping" % line.strip()
+                invdata = {}
+                continue
+            if svtype == "inversion_partial" and refstart < invdata["refstart"] :
+                #in this case, refstart is correct (only one choice), just need to fix refstop
+                refstop    = invdata["refstop"]
+                refstoppos = invdata["refstoppos"] 
+                refstopidx = invdata["refstopidx"]
+            elif svtype == "inversion_paired" and min(refstart,refstop) < min(invdata["refstart"], invdata["refstop"]) :
+                #need to sort refstart/refstop here, and invdata[stop] is refstop
+                refstart    = min(refstart   , refstop   );
+                refstartpos = min(refstartpos, refstoppos);
+                refstartidx = min(refstartidx, refstopidx);
+                refstop    = invdata["refstop"]
+                refstoppos = invdata["refstoppos"]
+                refstopidx = invdata["refstopidx"]
+            elif svtype == "inversion_paired" :
+                #still need to sort for stop, and invdata[start] is start
+                refstop    = max(refstart   , refstop   );
+                refstoppos = max(refstartpos, refstoppos);
+                refstopidx = max(refstartidx, refstopidx);
+                refstart    = invdata["refstart"]
+                refstartpos = invdata["refstartpos"] 
+                refstartidx = invdata["refstartidx"] 
+            else : #inversion_partial
+                #in this case, need to move start to stop, and invdata[start] is start 
+                refstop     = refstart
+                refstoppos  = refstartpos
+                refstopidx  = refstartidx
+                refstart    = invdata["refstart"]
+                refstartpos = invdata["refstartpos"] 
+                refstartidx = invdata["refstartidx"] 
+
+            #svlen is estimate of size of inverted region (should be over-estimate)
+            svlen = round(refstop-refstart)
+
+            docoord = True            
+            invdata = {}
+
+        elif svtype.startswith("duplication") : #all duplications should be same
+
+            if refstart > refstop : #just in case
+                a = refstart
+                refstart = refstop
+                refstop = a
+                a = refstartpos
+                refstartpos = refstoppos
+                refstoppos = a
+            
+            svlen = round(refstop-refstart)
+
+            docoord = True            
+            
+        #end if for svtype
+
+        if docoord : #this applies to translocations, inversions, and duplications
             if refstart == refstartpos : #no misresolved site
                 prevrefstart = refcmap[ref][refstartidx-2]
                 nextrefstart = refcmap[ref][refstartidx] #list is 0-indexed while the ID in smap is 1-indexed, so +1 is implied
@@ -328,12 +427,13 @@ def smap_to_vcf(smappath, refcmap, xmappath, sample, output_prefix, ref_accessio
             end = refstop
             ciend = round( (nextrefstop - prevrefstop)/2 )
 
-        svlen = default_svlen[svtype]
-        if svlen is None:
-            #note: two inner abs should not be necessary
-            svlen = int(round( abs( abs(refstop-refstart) - abs(qrystop-qrystart) ) )) * (1. if svtype == "insertion" else -1.)
+            docoord = False
 
-        nent += 1
+        #move svlen computation up
+        #svlen = default_svlen[svtype]
+        #if svlen is None:
+            #note: two inner abs should not be necessary
+            #svlen = int(round( abs( abs(refstop-refstart) - abs(qrystop-qrystart) ) )) * (1. if svtype == "insertion" else -1.)
 
         if human_bool:
             if ref==23:
@@ -346,13 +446,20 @@ def smap_to_vcf(smappath, refcmap, xmappath, sample, output_prefix, ref_accessio
             elif ref1==24:
                 ref1='Y'
 
-        if svtype=="insertion" or svtype=="deletion":
+        if svtype=="insertion" or svtype=="deletion" or svtype.startswith("inversion") or svtype.startswith("duplication"):
             fout.write(("chr%s\t%i\tSMAP%s\tN\t<%s>\t%s\tPASS\tSVTYPE=%s"+si+"END=%i"+si+"SVLEN=%i"+si+"CIPOS=-%i,%i"+si+"CIEND=-%i,%i"+si+"EXPERIMENT=%s") % (ref, pos, smapid,vcftype, conf, vcftype, end, svlen, cipos, cipos, ciend, ciend, exp_id))
         elif svtype=="translocation_intrachr" or svtype=="translocation_interchr":
             fout.write(("chr%s\t%i\tSMAP%s\tN\t<%s>\t%s\tPASS\tSVTYPE=%s"+si+"CHR2=chr%s"+si+"END=%i"+si+"SVLEN=%i"+si+"CIPOS=-%i,%i"+si+"CIEND=-%i,%i"+si+"CT=%s"+si+"EXPERIMENT=%s") % (ref, pos, smapid,vcftype, conf, vcftype, ref1, end, svlen, cipos, cipos, ciend, ciend, orientation, exp_id))
+        #elif svtype == "inversion_partial" or svtype == "inversion_paired" :
+        #    fout.write(("chr%s\t%i\tSMAP%s\tN\t<%s>\t%s\tPASS\tSVTYPE=%s"+si+"END=%i"+si+"SVLEN=%i"+si+"CIPOS=-%i,%i"+si+"CIEND=-%i,%i"+si+"EXPERIMENT=%s") % (ref, pos, smapid, vcftype, conf, vcftype, end, svlen, cipos, cipos, ciend, ciend, exp_id))
+        else :
+            print "WARNING: unhandled smap type (entry %s); skipping" % line.strip()
+            continue
         if dogeno :
             fout.write("\tGT\t%s" % gt)
         fout.write("\n")
+
+        nent += 1
 
     #end loop on input smap
     f1.close()
@@ -364,12 +471,14 @@ def smap_to_vcf(smappath, refcmap, xmappath, sample, output_prefix, ref_accessio
 def getArgs() :
     parser = argparse.ArgumentParser(description=description)
 
-	#required input
+    #required input
     parser.add_argument('-s', dest='smappath', help='Path to smap file to convert (required)', type=str)
-    parser.add_argument('-r', dest='refcmappath', help='Path to reference cmap which is refered to in the smap supplied as -s argument (required)', type=str)
-    parser.add_argument('-x', dest='xmappath', help='Path to xmap which is refered to in the smap supplied as -s argument (required)', type=str)
+    #parser.add_argument('-r', dest='refcmappath', help='Path to reference cmap which is refered to in the smap supplied as -s argument (required)', type=str)
+    #parser.add_argument('-x', dest='xmappath', help='Path to xmap which is refered to in the smap supplied as -s argument (required)', type=str)
 
     #optional input
+    parser.add_argument('-r', dest='refcmappath', help='Path to reference cmap which is refered to in the smap supplied as -s argument (optional, default to _r.cmap in smap dir)', type=str, default="")
+    parser.add_argument('-x', dest='xmappath', help='Path to xmap which is refered to in the smap supplied as -s argument (optional, default to .xmap in smap dir)', type=str, default="")
     defsamp = "Sample1"
     parser.add_argument('-n', dest='sample', help='Sample ID name for genotype data (optional, default "%s")'%defsamp, type=str, default=defsamp)
     output_prefix = None
@@ -385,8 +494,12 @@ def getArgs() :
 
     #unpack
     smappath = result.smappath #required
-    refcmappath = result.refcmappath
-    xmappath = result.xmappath
+    if not checkFile(smappath,".smap") :
+        print "ERROR: smap does not exist, is not readable, or does not end with '.smap' (check -s argument):", smappath
+        return None
+
+    refcmappath = (result.refcmappath if result.refcmappath else smappath.replace(".smap","_r.cmap"))
+    xmappath    = (result.xmappath    if result.xmappath    else smappath.replace(".smap",".xmap"))
 
     sample = result.sample #optional
     output_prefix = result.output_prefix
@@ -395,10 +508,6 @@ def getArgs() :
     exp_id = result.exp_id
 
     #checking files
-    if not checkFile(smappath,".smap") :
-        print "ERROR: smap does not exist, is not readable, or does not end with '.smap':", smappath
-        return None
-
     if not checkFile(refcmappath,".cmap") :
         print "ERROR: reference cmap does not exist, is not readable, or does not end with '.cmap':", refcmappath
         return None
